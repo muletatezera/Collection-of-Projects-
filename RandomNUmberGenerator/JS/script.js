035966ec4854877cb3b34a09e9ae45ab3ccd5f55
muleta <muletatezera@gmail.com>
2026-03-28 21:08:59 +0300

    add the event listener

--- RandomNUmberGenerator/JS/script.js
+++ RandomNUmberGenerator/JS/script.js
@@ -3,6 +3,8 @@ const firstNumber = document.getElementById("firstNumber");
 const secondNumber = document.getElementById("secondNumber");
 const thirdNumber = document.getElementById("thirdNumber");
 btn.onclick = function () {
-  firstNumber.innerHTML = Math.floor(Math.random() * 10) + 5;
-  console.log(firstNumber);
+  firstNumber.innerHTML = Math.floor(Math.random() * 10);
+  secondNumber.innerHTML = Math.floor(Math.random() * 20);
+  thirdNumber.innerHTML = Math.floor(Math.random() * 30);
+  // console.log(firstNumber);
 };
