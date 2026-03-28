e1642c64a23a600552b76112e7e0863895f30f2f
muleta <muletatezera@gmail.com>
2026-03-28 21:03:15 +0300

    function declare

--- RandomNUmberGenerator/JS/script.js
+++ RandomNUmberGenerator/JS/script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("generate-btn");
 const firstNumber = document.getElementById("firstNumber");
+const secondNumber = document.getElementById("secondNumber");
+const thirdNumber = document.getElementById("thirdNumber");
 btn.onclick = function () {
   firstNumber.innerHTML = Math.floor(Math.random() * 10) + 5;
   console.log(firstNumber);
