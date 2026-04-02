0dfd78220c212c1934711b6f2735a839384b1cc1
muleta <muletatezera@gmail.com>
2026-04-02 21:55:47 +0300

    full refracted

--- Number Guessing Game/usingWindowPrompt/JS/script.js
+++ Number Guessing Game/usingWindowPrompt/JS/script.js
@@ -3,11 +3,12 @@ const maxNumber = 50;
 const answer = Math.floor(
   Math.random() * (maxNumber - minNumber + 1) + minNumber,
 );
-// console.log(answer);
+console.log(answer);
 let attempt = 0;
 let guess;
 let running = true;
 while (running) {
+  attempt++;
   guess = window.prompt(`inter a number ${minNumber}-${maxNumber}`);
   guess = Number(guess);
   console.log(typeof guess);
@@ -16,12 +17,11 @@ while (running) {
   } else if (guess < minNumber || guess > maxNumber) {
     window.alert(`please inter valid number `);
   } else {
-    attempt++;
     if (guess < answer) {
       window.alert(`Too low! try again`);
     } else if (guess > answer) {
       window.alert(`Too high! try again`);
-    } else if (guess == answer) {
+    } else {
       window.alert(
         `Correct! the answer was ${answer} it tooks you ${attempt} to get`,
       );
